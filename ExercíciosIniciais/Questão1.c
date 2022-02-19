c217fb94ccd1c249a89d2e50a5ab952ffb5b0f0e
Naira Ingrid <88935608+NairaIngrid@users.noreply.github.com>
2022-02-19 14:28:11 +0000

    Desenvolvimento da questão 1

---
+++ ExercíciosIniciais/Questão1.c
@@ -0,0 +1,12 @@
+//Faça um programa que imprima o seu nome, sua matrícula, e o seu semestre de ingresso no curso. Cada informação em uma linha.
+
+
+#include <stdio.h>
+
+int main (){
+
+  printf("Nome: Naira Ingrid\n");
+  printf("Matrícula: 20201160015\n");
+  printf("Semestre de ingresso no curso:2020.1\n");
+   
+}
