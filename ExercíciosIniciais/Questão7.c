be251ec8379f2b7a3877def286e48c644f5cf03a
Naira Ingrid <88935608+NairaIngrid@users.noreply.github.com>
2022-02-19 16:43:26 +0000

    Desenvolvimento da questão 7

---
+++ ExercíciosIniciais/Questão7.c
@@ -0,0 +1,21 @@
+//Faça um programa que verifique se um número é primo -divisível por 1 e por ele mesmo.
+
+#include <stdio.h>
+
+int main(){
+
+    int n, i, qtdeDivisores;
+        
+    printf ("Digite um número:\n");
+    scanf("%d",&n);
+    
+    for(i = 1, qtdeDivisores = 0; i <= n; i++)
+        if((n%i) == 0)
+                qtdeDivisores++;
+           
+        if(qtdeDivisores == 2)
+            printf("%d é um número primo. ", n);
+        else
+            printf("%d não é um número primo. ", n);
+    
+}
