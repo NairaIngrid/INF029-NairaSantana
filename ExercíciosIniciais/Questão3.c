fcb0a4cd5b2dca3e81e61a29b4a7e668db935ba5
Naira Ingrid <88935608+NairaIngrid@users.noreply.github.com>
2022-02-19 15:11:45 +0000

    Desenvolvimento da questão 3

---
+++ ExercíciosIniciais/Questão3.c
@@ -0,0 +1,21 @@
+//Faça um programa que leia dois valores informados pelo usuário e inverta os valores nas variáveis lidas
+
+#include <stdio.h>
+
+int main () {
+
+  int n1, n2, aux;
+
+  printf("Informe o valor do primeiro número:\n");
+  scanf("%d", &n1);
+
+  printf("Informe o valor do segundo número:\n");
+  scanf("%d", &n2);
+
+  aux = n1;
+  n1 = n2;
+  n2 = aux;
+
+  printf("n1= %d e n2 = %d", n1,n2);
+  
+}
