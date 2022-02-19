99f0f0a2501a2a893c70cd5049c1f1aa0466324a
Naira Ingrid <88935608+NairaIngrid@users.noreply.github.com>
2022-02-19 17:04:16 +0000

    Desenvolvimento da questão 8

---
+++ ExercíciosIniciais/Questão8.c
@@ -0,0 +1,20 @@
+//Faça um programa que calcula o fatorial de um número - multiplicação pelos antecessores do número até chegar a 1.
+
+#include <stdio.h>
+
+int main()
+{
+  int fat, n;
+  
+  printf("Insira um valor:\n");
+  scanf("%d", &n);
+  
+  if(n == 1){
+    printf("O fatorial é: 1.");
+  }else{
+    for(fat = 1; n > 1; n--)
+    fat = fat * n;
+    printf("O fatorial é: %d", fat);
+  }
+   
+}
