992aef9e6208f4ea7ab7a475dcd2ba88febd1273
Naira Ingrid <88935608+NairaIngrid@users.noreply.github.com>
2022-02-19 16:10:28 +0000

    Desenvolvimento da questão 6

---
+++ ExercíciosIniciais/Questão6.c
@@ -0,0 +1,29 @@
+//Faça um programa que leia a idade de uma pessoa e informe de ela é de maior ou de menor. O programa deve repetir essa ação até que uma idade negativa seja informada
+
+#include <stdio.h>
+
+int main () {
+
+  int idade;
+
+
+  do {
+    
+    printf("Informe a sua idade:\n");
+    scanf("%d", &idade);
+    
+    if (idade>0){
+
+      if(idade>=18){
+      printf("Você é maior de idade!\n");
+      }else{
+      printf("Você ainda não atingiu a maior idade!\n");
+    }
+      }else{
+        printf("Idade nula ou negativa. Programa finalizado.");
+      }
+    }
+    
+   while (idade>0);
+  
+  }
